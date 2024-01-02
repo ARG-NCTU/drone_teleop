983e4d055a8d89459a004a5ffe9fa3371cc4e1ef
wellyowo <welly.ee11@nycu.edu.tw>
2024-01-02 23:56:02 +0800

    Fix: update drone F710 joy control with set px4 parameter for testing

--- src/drone_teleop.cpp
+++ src/drone_teleop.cpp
@@ -1,3 +1,4 @@
+/// Drone F710 joy control need switch to D mode!!!
 #include <iostream>
 #include <math.h> 
 
@@ -8,9 +9,10 @@
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/CommandTOL.h>
+#include <mavros_msgs/ParamGet.h>
+#include <mavros_msgs/ParamSet.h>
 
 using namespace std; 
-
 
 class Controller{
     private:
@@ -21,8 +23,11 @@ class Controller{
         ros::ServiceClient srv_arming;
         ros::ServiceClient srv_offboard;
         ros::ServiceClient srv_land;
+        ros::ServiceClient srv_hover;
+        ros::ServiceClient srv_param_set_;
+        ros::ServiceClient srv_param_get_;
 
-
+        //for suckion gripper
         ros::ServiceClient srv_gripper0_on;
         ros::ServiceClient srv_gripper1_on;
         ros::ServiceClient srv_gripper2_on;
@@ -46,11 +51,11 @@ class Controller{
 
 
         float m_highspeed_linear = 3.0;
-        float m_highspeed_trottle = 2.0;
+        float m_highspeed_throttle = 2.0;
         float m_highspeed_angular = 1;
 
         float m_lowspeed_linear = 1.0;
-        float m_lowspeed_trottle = 1.0;
+        float m_lowspeed_throttle = 1.0;
         float m_lowspeed_angular = 0.5;
 
         bool enable_joy_output = true;
@@ -68,6 +73,7 @@ class Controller{
         bool srvArming();
         bool srvOffboard();
         bool srvLand();
+        bool srvHover();
         bool srvGripper(bool open);
         void control();
 };
@@ -80,7 +86,11 @@ Controller :: Controller(){
     srv_arming = n.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
     srv_offboard = n.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
     srv_land = n.serviceClient<mavros_msgs::CommandTOL>("mavros/cmd/land");
-
+    srv_hover = n.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
+    srv_param_set_ = n.serviceClient<mavros_msgs::ParamSet>("/mavros/param/set");
+    srv_param_get_ = n.serviceClient<mavros_msgs::ParamGet>("/mavros/param/get");
+    
+    //for suckion gripper
     srv_gripper0_off = n.serviceClient<std_srvs::Empty>("drone_gripper/g0/off");
     srv_gripper1_off = n.serviceClient<std_srvs::Empty>("drone_gripper/g1/off");
     srv_gripper2_off = n.serviceClient<std_srvs::Empty>("drone_gripper/g2/off");
@@ -104,11 +114,11 @@ Controller :: Controller(){
 
 void Controller :: getParam(){
     n.getParam("/drone_teleop/highspeed_linear",  m_highspeed_linear);
-    n.getParam("/drone_teleop/highspeed_trottle", m_highspeed_trottle);
+    n.getParam("/drone_teleop/highspeed_throttle", m_highspeed_throttle);
     n.getParam("/drone_teleop/highspeed_angular", m_highspeed_angular);
 
     n.getParam("/drone_teleop/lowspeed_linear",  m_lowspeed_linear);
-    n.getParam("/drone_teleop/lowspeed_trottle", m_lowspeed_trottle);
+    n.getParam("/drone_teleop/lowspeed_throttle", m_lowspeed_throttle);
     n.getParam("/drone_teleop/lowspeed_angular", m_lowspeed_angular);
     return;
 }
@@ -120,33 +130,62 @@ void Controller :: joyCallback(const sensor_msgs::Joy::ConstPtr& msg){
 
     // mode & arming
     bool state = 0;
-    
-    if(m_buttons[1] == 1){
+    if(m_buttons[0] == 1){ // joy x button
+        while(!state){ state = srvHover(); }
+    }
+    if(m_buttons[1] == 1){ // joy A button
+        //add set param
+        if (srv_param_set_.exists() && srv_param_get_.exists())
+        {
+            mavros_msgs::ParamSet srv_param_set;
+            srv_param_set.request.param_id = "COM_RCL_EXCEPT";
+            srv_param_set.request.value.integer = 4;
+            srv_param_set_.call(srv_param_set);
+
+            mavros_msgs::ParamGet srv_param_get;
+            srv_param_get.request.param_id = "COM_RCL_EXCEPT";
+            srv_param_get_.call(srv_param_get);
+            ROS_INFO_STREAM("Set COM_RCL_EXCEPT: " << srv_param_set.request.value.integer
+                                                   << ". Get COM_RCL_EXCEPT: " << srv_param_get.response.value.integer);
+
+            srv_param_set.request.param_id = "COM_OBL_ACT";
+            srv_param_set.request.value.integer = -1;
+            srv_param_set_.call(srv_param_set);
+
+            srv_param_get.request.param_id = "COM_OBL_ACT";
+            srv_param_get_.call(srv_param_get);
+            ROS_INFO_STREAM("Set COM_OBL_ACT: " << srv_param_set.request.value.integer
+                                                << ". Get COM_OBL_ACT: " << srv_param_get.response.value.integer);
+        }
+        else
+        {
+            ROS_ERROR_STREAM("Param service does not exist");
+        }
         while(!state){ state = srvArming(); }
     }
     state = 0;
-    if(m_buttons[2] == 1){
+    if(m_buttons[2] == 1){ // joy B button
         while(!state){ state = srvOffboard(); }
     }
     state = 0;
-    if(m_buttons[3] == 1){
+    if(m_buttons[3] == 1){ // joy Y button
         while(!state){ state = srvLand(); }
     }
 
-    if(m_buttons[9] == 1){
+    if(m_buttons[9] == 1){ // joy start button
         enable_joy_output = false;
         ROS_INFO("Joystick enable: %s", enable_joy_output ? "true" : "false");
     }
 
-    if(m_buttons[8] == 1){
+    if(m_buttons[8] == 1){ // joy back button
         enable_joy_output = true;
         ROS_INFO("Joystick enable: %s", enable_joy_output ? "true" : "false");
     }
 
-    if(m_axes[5] == 1){
+    if(m_axes[5] == 1){ // joy arrow up
         srvGripper(true);
     }
-    if(m_axes[5] == -1){
+    if(m_axes[5] == -1){ // joy arrow down
         srvGripper(false);
     }
 
@@ -165,6 +204,14 @@ bool Controller :: srvOffboard(){
     ROS_INFO("Srv offboard");
     mavros_msgs::SetMode srv_msg;
     srv_msg.request.custom_mode = "OFFBOARD";
+    if(srv_offboard.call(srv_msg)){ return true; }
+    else{ return false; }
+}
+
+bool Controller :: srvHover(){
+    ROS_INFO("Srv Hover");
+    mavros_msgs::SetMode srv_msg;
+    srv_msg.request.custom_mode = "AUTO.LOITER";
     if(srv_offboard.call(srv_msg)){ return true; }
     else{ return false; }
 }
@@ -230,27 +277,31 @@ void Controller :: control(){
         geometry_msgs::Twist pub_msg_twist;
         // {rotate, up/down, left/right shift, forward/backward shift, 0, 0}
 
-        float trottle, linear, angular;
+        float throttle, linear, angular;
 
-        if(m_buttons[5] == 1){
-            trottle = m_lowspeed_trottle;
+        if(m_buttons[5] == 1){ // joy RB button
+            throttle = m_lowspeed_throttle;
             linear  = m_lowspeed_linear;
             angular = m_lowspeed_angular; 
         }else{
-            trottle = m_highspeed_trottle;
+            throttle = m_highspeed_throttle;
             linear  = m_highspeed_linear;
             angular = m_highspeed_angular;
         }
-        if(m_buttons[4] == 1){
+        if(m_buttons[4] == 1){ //joy LB button
             pub_msg_twist.angular.z = angular * m_axes[0];
-            pub_msg_twist.linear.z = trottle * m_axes[1];
+            pub_msg_twist.linear.z = throttle * m_axes[1];
 
             pub_msg_twist.linear.y = linear * m_axes[2];
             pub_msg_twist.linear.x = linear * m_axes[3];
-            
-
-            pub_twist.publish(pub_msg_twist);
         }
+        else{
+            pub_msg_twist.angular.z = 0.0;
+            pub_msg_twist.linear.z = 0.0;
+            pub_msg_twist.linear.y = 0.0;
+            pub_msg_twist.linear.x = 0.0;
+        }
+        pub_twist.publish(pub_msg_twist);
         
     }
     rate.sleep();
