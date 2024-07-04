ed069e7e302cb6c6ec83cd1cc26302c4852de68e
wellyowo <welly.ee11@nycu.edu.tw>
2024-07-04 22:07:11 +0800

    Feat: add docstring for drone_vr_teleop and add high low speed switch

--- src/drone_vr_teleop.cpp
+++ src/drone_vr_teleop.cpp
@@ -4,6 +4,7 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
 #include <geometry_msgs/Twist.h>
+#include <std_msgs/Bool.h>
 #include <std_srvs/Empty.h>
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/SetMode.h>
@@ -13,6 +14,40 @@
 #include <mavros_msgs/ParamSet.h>
 using namespace std; 
 
+/**
+ * @file drone_vr_teleop.cpp
+ * @brief ROS node for controlling a drone using VR joystick inputs.
+ * 
+ * This code defines a Controller class to manage drone teleoperation via VR joystick inputs.
+ * 
+ * Functions:
+ *  - Controller::Controller: Constructor that initializes the ROS node handles, subscribers, publishers, and service clients.
+ *  - Controller::getParam: Retrieves parameters from the ROS parameter server.
+ *  - Controller::joyCallback: Callback function for joystick messages. Processes the joystick input and triggers corresponding actions.
+ *  - Controller::toggleOutput: Toggles the joystick output state.
+ *  - Controller::srvArming: Arms the drone.
+ *  - Controller::srvOffboard: Sets the drone to offboard mode.
+ *  - Controller::srvLand: Lands the drone.
+ *  - Controller::srvHover: Sets the drone to hover mode.
+ *  - Controller::srvFlypose: Sends a trigger to fly to the initial pose.
+ *  - Controller::control: Controls the drone based on the joystick inputs.
+ * 
+ * ROS Subscribers:
+ *  - sub_joy: Subscribes to "vr_joy_drone" topic to receive joystick inputs.
+ * 
+ * ROS Publishers:
+ *  - pub_twist: Publishes twist messages to the "drone_twist" topic to control the drone's movement.
+ *  - pub_speed_state: Publishes boolean messages to the "speed_state" topic to indicate high/low speed state.
+ * 
+ * ROS Service Clients:
+ *  - srv_arming: Client for "mavros/cmd/arming" service to arm the drone.
+ *  - srv_offboard: Client for "mavros/set_mode" service to set the drone mode to offboard.
+ *  - srv_land: Client for "mavros/cmd/land" service to land the drone.
+ *  - srv_hover: Client for "mavros/set_mode" service to set the drone mode to hover.
+ *  - srv_flypose: Client for "/flypose" service to send a trigger to fly to the initial pose.
+ *  - srv_param_set_: Client for "/mavros/param/set" service to set parameters.
+ *  - srv_param_get_: Client for "/mavros/param/get" service to get parameters.
+ */
 
 class Controller{
     private:
@@ -20,6 +55,8 @@ class Controller{
         ros::NodeHandle nh_private;
         ros::Subscriber sub_joy;
         ros::Publisher pub_twist;
+        ros::Publisher pub_speed_state;
+        ros::Publisher pub_enable_auto_mode_success_; //publish bt condition node of auto switch (by operator)
 
         ros::ServiceClient srv_arming;
         ros::ServiceClient srv_offboard;
@@ -39,7 +76,10 @@ class Controller{
         float m_lowspeed_angular = 0.5;
 
         bool enable_joy_output = true;
+        bool enable_auto_mode_success_ = false;
         bool mode_switch = false;
+
+        bool high_speed = false;
 
         // {vr trigger + left_hand mushroom back/forth (For up/down), vr trigger + left_hand mushroom left/right (For rotate), vr trigger + left_hand mushroom back/forth (For move back and forth), vr gripper + left_hand mushroom left/right (For move left and right)}
         float m_axes[4] = {0.0, 0.0, 0.0, 0.0};
@@ -63,6 +103,8 @@ class Controller{
 Controller :: Controller(): nh_private("~"){
     sub_joy = n.subscribe<sensor_msgs::Joy>("vr_joy_drone", 1,  &Controller::joyCallback, this);
     pub_twist = n.advertise<geometry_msgs::Twist>("drone_twist", 10);
+    pub_speed_state = n.advertise<std_msgs::Bool>("speed_state", 10);//True for high speed, False for low speed
+    pub_enable_auto_mode_success_ = n.advertise<std_msgs::Bool>("/enable_auto_mode_success", 10);// True for auto mode enabled, False for auto mode disabled
     
 
     srv_arming = n.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
@@ -108,21 +150,21 @@ void Controller :: joyCallback(const sensor_msgs::Joy::ConstPtr& msg){
     }
     allZero = false;
     // If all data is zero, switch to position mode, otherwise switch to offboard mode
-    if(mode_switch && allZero){
-        ROS_INFO("Switching to Position Mode");
-        mavros_msgs::SetMode positionModeSrv;
-        positionModeSrv.request.custom_mode = "POSCTL"; 
-        if(srv_hover.call(positionModeSrv)){
-            ROS_INFO("Position mode set successfully");
-        }else{
-            ROS_ERROR("Failed to set position mode");
-        }
-    }else{
-        ROS_INFO("Switching to Offboard Mode");
-        if(!srvOffboard()){
-            ROS_ERROR("Failed to set offboard mode");
-        }
-    }
+    // if(mode_switch && allZero){
+    //     ROS_INFO("Switching to Position Mode");
+    //     mavros_msgs::SetMode positionModeSrv;
+    //     positionModeSrv.request.custom_mode = "POSCTL"; 
+    //     if(srv_hover.call(positionModeSrv)){
+    //         ROS_INFO("Position mode set successfully");
+    //     }else{
+    //         ROS_ERROR("Failed to set position mode");
+    //     }
+    // }else{
+    //     ROS_INFO("Switching to Offboard Mode");
+    //     if(!srvOffboard()){
+    //         ROS_ERROR("Failed to set offboard mode");
+    //     }
+    // }
 
     
     if(m_buttons[0] == 1){
@@ -172,11 +214,11 @@ void Controller :: joyCallback(const sensor_msgs::Joy::ConstPtr& msg){
 
     if(m_buttons[3] == 1){
         //VR right hand button B push count
-        enable_joy_output = false; 
+        enable_auto_mode_success_ = true; 
         
     }
     else if(m_buttons[3] == 0){
-        enable_joy_output = true;
+        enable_auto_mode_success_ = false;
     }
 
     return;
@@ -249,10 +291,12 @@ void Controller :: control(){
             throttle = m_lowspeed_throttle;
             linear  = m_lowspeed_linear;
             angular = m_lowspeed_angular;
+            high_speed = false;
         }else{
             throttle = m_highspeed_throttle;
             linear  = m_highspeed_linear;
             angular = m_highspeed_angular;
+            high_speed = true;
         }
         
 
@@ -262,6 +306,13 @@ void Controller :: control(){
         pub_msg_twist.linear.y = m_axes[3] * linear;
         //ROS_INFO("Speed: %f, %f, %f, %f", pub_msg_twist.linear.x, pub_msg_twist.linear.y, pub_msg_twist.linear.z, pub_msg_twist.angular.z);
         pub_twist.publish(pub_msg_twist);
+        std_msgs::Bool speed_state;
+        speed_state.data = high_speed;//true for high speed, false for low speed
+        pub_speed_state.publish(speed_state);
+        std_msgs::Bool enable_auto_mode;
+        enable_auto_mode.data = enable_auto_mode_success_;
+        pub_enable_auto_mode_success_.publish(enable_auto_mode);
+
     }
     rate.sleep();
     return;
